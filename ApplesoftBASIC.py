3710826d74cd68ebfe2ba0dd7be28634ea02f421
Emil Zanettin <emilz@blackmagicdesign.com>
2023-06-09 12:21:02 +1000

    Lots of changes. Got nervous and added global everywhere; doubled screen size; added inverse() and normal(); hgr() moves cursor to line 21 (maybe not right); print() scrolls at end

--- ApplesoftBASIC.py
+++ ApplesoftBASIC.py
@@ -2,6 +2,11 @@ import time
 import pygame
 
 
+def render():
+    pygame.transform.scale(gAppleDisplaySurface, gMainDisplaySize, gMainDisplaySurface)
+    pygame.display.update()
+  
+
 def sgn(num):
     if num < 0:
         return -1
@@ -12,6 +17,8 @@ def sgn(num):
 
 
 def htab(x):
+    global gCursor
+
     if x < 1:
         x = 1
     elif x > 40:
@@ -20,6 +27,8 @@ def htab(x):
 
 
 def vtab(y):
+    global gCursor
+
     if y < 1:
         y = 1
     elif y > 40:
@@ -28,71 +37,109 @@ def vtab(y):
 
 
 def setTextWindowTop(y):
+    global gCursor
+
+    y = y + 1           # user specifies line before they line the want in the window (ie, default if 0)
     gCursor["minY"] = y
     if gCursor["y"] < y:
         gCursor["y"] = y
 
 
 def setTextWindowBottom(y):
+    global gCursor
+
     gCursor["maxY"] = y
     if gCursor["y"] > y:
         gCursor["y"] = y + 1     # ready to scroll
 
 
+def setTextWindowRight(x):
+    global gCursor
+
+    gCursor["maxX"] = x
+    if gCursor["x"] > x:
+        gCursor["x"] = 1
+
 def home():
+    global gCursor
+
     x = (gCursor["minX"] - 1) * 7
     width = (gCursor["maxX"] - gCursor["minX"] + 1) * 7
     y = (gCursor["minY"] - 1) * 8
     height = (gCursor["maxY"] - gCursor["minY"] + 1) * 8
 
     rect = pygame.Rect(x, y, width, height)
-    pygame.draw.rect(gDisplaySurface, [0, 0, 0], rect)
+    pygame.draw.rect(gAppleDisplaySurface, [0, 0, 0], rect)
 
     gCursor["x"] = gCursor["minX"]
     gCursor["y"] = gCursor["minY"]
 
 
+def inverse():
+    global gInverseText
+    gInverseText = True
+
+
+def normal():
+    global gInverseText
+    gInverseText = False
+
+
 def hgr():
+    global gCursor
+
     rect = pygame.Rect(0, 0, 280, 160)
-    pygame.draw.rect(gDisplaySurface, [0, 0, 0], rect)
+    pygame.draw.rect(gAppleDisplaySurface, [0, 0, 0], rect)
+    gCursor["minY"] = 21; gCursor["maxY"] = 24
 
 
 def clearLineEnd():
+    global gCursor
+
     x = (gCursor["x"] - 1) * 7
     width = (gCursor["maxX"] - gCursor["x"] + 1) * 7
     y = (gCursor["y"] - 1) * 8
     height = 8
     rect = pygame.Rect(x, y, width, height)
-    pygame.draw.rect(gDisplaySurface, [0, 0, 0], rect)
+    pygame.draw.rect(gAppleDisplaySurface, [0, 0, 0], rect)
 
 
 def scroll():
+    global gCursor
+
     x = (gCursor["minX"] - 1) * 7
     width = (gCursor["maxX"] - gCursor["minX"] + 1) * 7
     y = (gCursor["minY"] - 1) * 8
     height = (gCursor["maxY"] - gCursor["minY"] + 1) * 8
 
     clipRect = pygame.Rect(x, y, width, height)
-    gDisplaySurface.set_clip(clipRect)
-    gDisplaySurface.scroll(0, -8)
+    gAppleDisplaySurface.set_clip(clipRect)
+    gAppleDisplaySurface.scroll(0, -8)
 
     blankRect = pygame.Rect(x, (gCursor["maxY"] - 1) * 8, width, 8)
-    pygame.draw.rect(gDisplaySurface, [0, 0, 0], blankRect)
+    pygame.draw.rect(gAppleDisplaySurface, [0, 0, 0], blankRect)
 
-    gDisplaySurface.set_clip(gDisplaySurface.get_rect())
+    gAppleDisplaySurface.set_clip(gAppleDisplaySurface.get_rect())
 
 
 def drawText(x, y, text):
-    image = gMainFont.render(text, True, [255, 255, 255], [0, 0, 0])
+    global gInverseText
+
+    if gInverseText:
+        foreColour = [0, 0, 0]
+        backColour = [255, 255, 255]
+    else:
+        foreColour = [255, 255, 255]
+        backColour = [0, 0, 0]
+
+    image = gMainFont.render(text, True, foreColour, backColour)
     rect = image.get_rect()
     rect.topleft = [(x - 1) * 7, (y - 1) * 8]
-    gDisplaySurface.blit(image, rect)
+    gAppleDisplaySurface.blit(image, rect)
 
 
 def print(text, newLine = True):
-    if gCursor["y"] > gCursor["maxY"]:
-        scroll()
-        gCursor["y"] = gCursor["maxY"]
+    global gCursor
 
     x = gCursor["x"]
     y = gCursor["y"]
@@ -100,12 +147,17 @@ def print(text, newLine = True):
 
     if newLine:
         gCursor["x"] = gCursor["minX"]
-        gCursor["y"] = y + 1
+        if y < gCursor["maxY"]:
+            gCursor["y"] = y + 1
+        else:
+            scroll()
+            gCursor["y"] = gCursor["maxY"]
     else:
         gCursor["x"] = x + len(text)
 
 
 def input(promptText):
+    global gCursor
     text = ""
     clock = pygame.time.Clock()
     dt = 0
@@ -113,7 +165,8 @@ def input(promptText):
 
     print(promptText, False)
     clearLineEnd()
-    x = gCursor["x"]; y = gCursor["y"]
+    x = gCursor["x"]
+    y = gCursor["y"]
 
     while not done:
         for event in pygame.event.get():
@@ -129,7 +182,7 @@ def input(promptText):
                     text += event.unicode
 
         drawText(x, y, f"{text} ")
-        pygame.display.update()
+        render()
         dt = clock.tick(30) / 1000
 
     gCursor["x"] = 1
@@ -142,7 +195,7 @@ def get():
     clock = pygame.time.Clock()
     dt = 0
 
-    pygame.display.update()        
+    render()        
 
     while True:
         for event in pygame.event.get():
@@ -160,7 +213,7 @@ def getKeypress():
     clock = pygame.time.Clock()
     dt = 0
 
-    pygame.display.update()        
+    render()        
 
     while True:
         for event in pygame.event.get():
@@ -177,8 +230,11 @@ def getKeypress():
 
  
 gCursor = { "x": 1, "y": 1, "minX": 1, "minY": 1, "maxX": 40, "maxY": 24 }
+gMainDisplaySize = (560, 384)
   
 # create the display surface object of specific dimension..e(X, Y).
 pygame.init()
-gDisplaySurface = pygame.display.set_mode((280, 192))
+gAppleDisplaySurface = pygame.Surface((280,192))
+gMainDisplaySurface = pygame.display.set_mode(gMainDisplaySize)
 gMainFont = pygame.font.Font('PrintChar21.ttf', 8)
+gInverseText = False
